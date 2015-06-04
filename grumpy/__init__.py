63b683b6078277437827cc51387a4a7ee0701bac
Steve King, Jr <stevekx86@gmail.com>
2015-06-04 13:20:53 -0700

    I'm an idiot when it comes to __init__

--- grumpy/__init__.py
+++ grumpy/__init__.py
@@ -1,10 +1,1 @@
 from grumpy import *
-
-def generate_token(user_identifier, creation_datetime, secret):
-	pass
-	
-def generate_secret(bits):
-	pass
-	
-def verify_token(token, user_identifier, creation_datetime, secret):
-	pass
