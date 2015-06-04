8bec29db08b6dbb935d12c9358e254c90350c0d5
Steve King, Jr <stevekx86@gmail.com>
2015-06-04 12:24:32 -0700

    Generates cryptographically secure sequences based

--- grumpy/__init__.py
+++ grumpy/__init__.py
@@ -1,10 +1,17 @@
 from datetime import datetime
+from os import urandom
 
 def token(user_identifier, creation_datetime, secret):
 	return ""
 	
 def generate_secret(bits):
-	return ""
+	if not bits.is_integer():
+		raise ValueError("Bits must be an integer")  # The programmer is an ass
+	elif bits % 8 != 0:
+		raise ValueError("Bits not divisible by 8")  # The programmer is an idiot
+	else:
+		bytes = bits / 8
+		return int(urandom(bytes).encode("hex"), 16) # The programmer is a really cool guy
 
 def verify_token(token, user_identifier, creation_datetime, secret):
 	return ""
