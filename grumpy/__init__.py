fb426d551dc073797cb5b50bdbd2494696c9e82f
Steve King, Jr <stevekx86@gmail.com>
2015-06-04 12:12:47 -0700

    Stub methods

---
+++ grumpy/__init__.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def token(user_identifier, creation_datetime, secret):
+	return ""
+	
+def generate_secret(bits):
+	return ""
+
+def verify_token(token, user_identifier, creation_time, secret):
+	return ""
