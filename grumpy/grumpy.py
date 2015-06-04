18739487bdded2984fb83cad22dc32572015b993
Steve King, Jr <stevekx86@gmail.com>
2015-06-04 13:00:43 -0700

    Better project mapping.

---
+++ grumpy/grumpy.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from os import urandom
+from hashlib import sha256
+import hmac
+import base64
+
+def generate_token(user_identifier, creation_datetime, secret):
+	creation_unix_timestamp = creation_datetime.strftime("%s")
+	token_identifier = "{0}:{1}".format(user_identifier, creation_unix_timestamp)
+	token_hmac = base64.b64encode(hmac.new(token_identifier, secret, digestmod=sha256).digest())
+	token = "{0}:{1}".format(token_identifier, token_hmac)
+	return token
+	
+def generate_secret(bits):
+	if not bits.is_integer():
+		raise ValueError("Bits must be an integer")  # The programmer is an ass
+	elif bits % 8 != 0:
+		raise ValueError("Bits not divisible by 8")  # The programmer is an idiot
+	else:
+		bytes = bits / 8
+		return iurandom(bytes).encode("hex")         # The programmer is a really cool guy
+
+def verify_token(token, user_identifier, creation_datetime, secret):
+	return generate_token(user_identifier, creation_datetime, secret) == token
