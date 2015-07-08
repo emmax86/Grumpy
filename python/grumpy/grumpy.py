10201f4bec0e9d057dcc77ad13558841b796f887
Steve King, Jr <stevekx86@gmail.com>
2015-07-07 23:10:36 -0700

    Renamed variable to avoid ambiguity with matching type.

--- python/grumpy/grumpy.py
+++ python/grumpy/grumpy.py
@@ -15,8 +15,8 @@ def generate_secret(bits):
 	if bits % 8 != 0:
 		raise ValueError("Bits not divisible by 8")  # The programmer is an idiot
 	else:
-		bytes = bits / 8
-		return urandom(bytes).encode("hex")         # The programmer is a really cool guy
+		num_bytes = bits / 8
+		return urandom(num_bytes).encode("hex")         # The programmer is a really cool guy
 
 def verify_token(token, user_identifier, creation_datetime, secret):
 	return generate_token(user_identifier, creation_datetime, secret) == token
